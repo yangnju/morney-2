67a4822e45cf4876f0baac9cf334aa1cda41e1f8
CaramelStark <449134875@qq.com>
2021-03-25 16:55:41 +0800

    增加底栏选中变色功能

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -6,22 +6,17 @@
 
 <script lang="ts">
 /* eslint-disable */
-const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-try {
-  importAll(require.context('../assets/icons', true, /\.svg$/));
-} catch (error) {
-  console.log(error);
-}
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
 export default {
   props: ['name'],
   name: 'Icon'
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .icon {
-  width: 1em;
-  height: 1em;
+  width: 1em; height: 1em;
   vertical-align: -0.15em;
   fill: currentColor;
   overflow: hidden;
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/tags" class="item">
+    <router-link to="/tags" class="item" active-class="selected">
       <Icon name="tags"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -33,6 +33,7 @@ nav {
   flex-direction: row;
   font-size: 12px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   > .item {
     padding: 2px 0;
     width: 33.33333%;
@@ -45,6 +46,10 @@ nav {
       width: 32px;
       height: 32px;
     }
+  }
+
+  > .item.selected {
+    color: red;
   }
 }
 </style>
