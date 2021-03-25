ff59be6f978ede440a73ccd204a677ceecd6b73b
CaramelStark <449134875@qq.com>
2021-03-25 12:43:25 +0800

    增加底栏样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link to="/money" class="item">
       <Icon name="money"/>
       记账
     </router-link>
-    |
-    <router-link to="/tags">
+    <router-link to="/tags" class="item">
       <Icon name="tags"/>
       标签
     </router-link>
-    |
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item">
       <Icon name="statistics"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -30,7 +28,23 @@ export default class Nav extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  border: 1px solid red;
+nav {
+  display: flex;
+  flex-direction: row;
+  font-size: 12px;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  > .item {
+    padding: 2px 0;
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
 }
 </style>
